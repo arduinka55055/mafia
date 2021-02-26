bdd9e7fedc6d24514ca9018bbfb19cd33d455579
Maksikos-Ctrl <maksikos973@gmail.com>
2021-02-26 11:32:42 +0200

    test

--- mainsrv.py
+++ mainsrv.py
@@ -1,5 +1,5 @@
-from flask import Flask,redirect,Response,request,render_template,make_response
-import socket
+from flask import Flask,redirect,Response,request,render_template,make_response # import framework flask
+import socket # for networking interface
 
 app=Flask(__name__)
 @app.route('/')
@@ -9,7 +9,7 @@ def main():
 @app.route('/<path>',methods=['GET','POST'])
 def pather(path):
     if path=='game':
-        return open("index.html",'rb').read()
+        return open("index.html",'rb.read()
     if path.endswith('zip'):
         return Response(open(path,'rb').read(),mimetype='application/zip')
     else:
