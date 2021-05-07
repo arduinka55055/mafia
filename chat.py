95820363c69012eb937fae2499fb256ffc9820be
Maksikos-Ctrl <maksikos973@gmail.com>
2021-05-07 11:56:24 +0300

    amfjadvjka

--- chat.py
+++ chat.py
@@ -1,13 +1,31 @@
-from flask import Flask
+from types import MethodDescriptorType
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_socketio import SocketIO, join_room, leave_room, emit, rooms
+from flask_session import Session
+from werkzeug import useragents
 
-chat = Flask(__name__)
 
-@app.route('/')
+app = Flask(__name__)
+app.debug = True
+app.config['SECRET_KEY'] = '.....'
+app.config['SESSION TYPE'] = 'filesystem'
+
+Session(app)
+
+@app.route('/', methods=['GET','POST' ])
 def index():
-    return "ЧТо-то"
+    return render_template("index.html")
+
+@app.route('/pregamechat', methods=['GET','POST'])
+def pregamechat():
+    if(request.method = 'POST'):
+        room = request.form['room'] 
+        code = request.form['code']  
+        #   
+          
 
 if __name__ == '__main__':  
-    chat.run()
+    app.run()
 
 
 
